607f724ac66aeca488a41cc248abffd9a94a3fe0
InspiratioNULL <206951839+InspiratioNULL@users.noreply.github.com>
2026-06-22 02:58:56 -0700

    Update LabsFX to version 2.0.0

--- Casks/labsfx.rb
+++ Casks/labsfx.rb
@@ -1,6 +1,6 @@
 cask "labsfx" do
-  version "1.69.9"
-  sha256 "6311f8eaa5d2abaae938a71024a7669ecbfd8436a4b0a4ed6ae7009a20353af5"
+  version "2.0.0"
+  sha256 "8a385955320b030e1e135d24abaefe7a32da7113446a05da9c16ce7b9d073ea0"
 
   url "https://github.com/InspiratioNULL/LabsFX-macOS/releases/download/#{version}/LabsFX.app.zip"
   name "LabsFX"
